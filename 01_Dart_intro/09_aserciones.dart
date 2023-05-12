915637de5a06687ab10a3e388fc4c46e1f532b81
Martin Gimenez <tinco94@gmail.com>
2023-05-12 12:31:43 -0300

    aserciones

---
+++ 01_Dart_intro/09_aserciones.dart
@@ -0,0 +1,34 @@
+void main() {
+  final windPlant = WindPlant( initialEnergy: 100);
+  
+  print(windPlant);
+  
+}
+
+enum PlantType { nuclear, wind, water }
+
+abstract class EnergyPlant {
+  
+  double energyLeft;
+  PlantType type; //nuclear, wind, water
+  
+  //constructor
+  EnergyPlant({ 
+    required this.energyLeft,
+    required this.type
+  });
+  
+  void consumeEnergy( double amount );
+  
+  
+}
+//extends, implements
+class WindPlant extends EnergyPlant{
+  WindPlant({ required double initialEnergy })
+    :super( energyLeft: initialEnergy, type: PlantType.wind );
+  
+  @override
+  void consumeEnergy( double amount ){
+    energyLeft -= amount;
+  }
+}
